8d487b2fa4e49c28a2687d5e7043984f7b9da538
shaoyahu <498486362@qq.com>
2025-09-11 00:00:17 +0800

    20250910

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { Button, notification } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 function App() {
-  const [count, setCount] = useState(0)
-
+  const openNotification = () => {
+    notification.open({
+      message: "Notification Title",
+      description:
+        "This is the content of the notification. This is the content of the notification. This is the content of the notification.",
+      onClick: () => {
+        console.log("Notification Clicked!");
+      },
+    });
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Button
+        className="!m-5"
+        type="primary"
+        onClick={openNotification}
+        icon={<SmileOutlined />}
+      >
+        Button
+      </Button>
+    </div>
+  );
 }
 
-export default App
+export default App;
